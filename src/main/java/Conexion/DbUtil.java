7ac60119d0dc71e967afe48af51787323ecc7854
Juan Manuel <Juan Manuel@DESKTOP-FSPC82F>
2017-11-16 11:19:37 -0500

    Ubicación usuario

--- src/main/java/Conexion/DbUtil.java
+++ src/main/java/Conexion/DbUtil.java
@@ -3,38 +3,37 @@ package Conexion;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class DbUtil {
-	private static Connection connection = null;
 
-    public static Connection getConnection() {
-        if (connection != null)
-            return connection;
-        else {
-            try {
-            	Properties prop = new Properties();
-                InputStream inputStream = DbUtil.class.getClassLoader().getResourceAsStream("/db.properties");
-                prop.load(inputStream);
-                String driver = prop.getProperty("driver");
-                String url = prop.getProperty("url");
-                String user = prop.getProperty("user");
-                String password = prop.getProperty("password");
-                Class.forName(driver);
-                connection = DriverManager.getConnection(url, user, password);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+    private static Connection CONEXION = null;
+
+    public static Connection getConnection() throws URISyntaxException {
+        if (CONEXION != null) {
+            return CONEXION;
+        } else {
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+            if (CONEXION == null) {
+                try {
+                    CONEXION = DriverManager.getConnection(dbUrl, username, password);
+                } catch (SQLException e) {
+                    System.out.println("Connection Failed! Check output console");
+                    e.printStackTrace();
+                }
+
             }
-            return connection;
+            return CONEXION;
+
         }
 
     }
